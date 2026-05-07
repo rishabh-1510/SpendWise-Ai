a298fb0845a74dad9deb82fb012629d0062bdfe2
rishabh-1510 <158290586+rishabh-1510@users.noreply.github.com>
2026-05-07 12:14:39 +0530

    Setup  Routes

--- spendwise-ai/src/App.tsx
+++ spendwise-ai/src/App.tsx
@@ -1,1 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import AuditForm from "./pages/AuditForm";
+import Results from "./pages/Results";
+import SharedReport from "./pages/SharedReport";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/audit" element={<AuditForm />} />
+        <Route path="/results" element={<Results />} />
+        <Route path="/report/:id" element={<SharedReport />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App;
